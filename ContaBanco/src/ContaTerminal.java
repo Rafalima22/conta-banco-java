c30b5c1fd5a760e9c8d9c989db72b6ff3369c3d4
Rafael Rodrigues <Rafaelrlima90@hotmail.com>
2025-11-22 17:23:12 -0300

    Estrtutura Bancária simples

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int Numero;
+        String Agencia;
+        String NomeDoCliente;
+        double Saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da conta: ");
+        Numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o numero da agencia: ");
+        Agencia = scanner.next();
+        
+        scanner.nextLine();
+        System.out.println("Por favor, digite o nome do cliente: ");
+        NomeDoCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        Saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ NomeDoCliente + " , o seu saldo é de R$ " + Saldo + " na conta de número " + Numero + " referente a agência " + Agencia + ".");
+
     }
 }
